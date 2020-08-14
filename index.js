edc26e5e5be1612bf753e74eb3f64620d95682e4
virtualcharms <sodalitemix@gmail.com>
2020-08-14 14:57:30 -0700

    Connect ends

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const cors = require('cors');
 const express = require('express');
 const showsController = require('./controllers/shows');
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
